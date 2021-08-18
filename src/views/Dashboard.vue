30eeda4ccaa8e95eac391c403b033fa0a582afcb
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-08-18 07:20:12 +0100

    dashboard header added

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,47 @@
   <div>
     <Sidebar />
     <NavBar />
+
+    <div class="dashboard">
+      <div class="dashboard-header d-flex align-items-center justify-content-between">
+        <h1>Payments</h1>
+
+        <!-- displays on small screen and above -->
+        <div class="d-sm-flex d-none d-sm-block">
+          <button class="d-flex align-items-center">
+            <img src="../assets/image/filter.svg" alt="filter icon">
+            Filter
+          </button>
+          <button class="d-flex align-items-center">
+            <img src="../assets/image/export.svg" alt="filter icon">
+            Export
+          </button>
+          <button class="d-flex align-items-center">
+            <img src="../assets/image/create-payment.svg" alt="filter icon">
+            Create Payment
+          </button>
+        </div>
+
+        <!-- displays on large screen and above -->
+        <div class="d-flex d-sm-none d-block">
+          <button class="d-flex align-items-center">
+            <img src="../assets/image/filter.svg" alt="filter icon">
+          </button>
+          <button class="d-flex align-items-center">
+            <img src="../assets/image/export.svg" alt="filter icon">
+          </button>
+          <button class="d-flex align-items-center">
+            <img src="../assets/image/create-payment.svg" alt="filter icon">
+          </button>
+        </div>
+      </div>
+
+      
+
+      
+
+      
+    </div>
   </div>
 </template>
 
