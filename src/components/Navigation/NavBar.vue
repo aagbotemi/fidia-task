d6a82abd9ea32a5a250f7a593fdc8f78f8296416
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-08-18 07:17:10 +0100

    navbar added

---
+++ src/components/Navigation/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="navbar">
+        <div class="form-group">
+            <input type="text" placeholder="Search...">
+            <img src="../../assets/image/search.svg" alt="search icon" width="13px" height="16px">
+        </div>
+
+        <div class="d-flex align-items-center">
+            <img src="../../assets/image/notification-icon.svg" alt="notification-icon" class="icon-img" width="20px" height="16px" >
+            <img src="../../assets/image/help-icon.svg" alt="help-icon" class="icon-img" width="20px" height="16px" >
+            <img src="../../assets/image/account-icon.svg" alt="account-icon" class="icon-img" width="20px" height="16px" >
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <Sidebar />
+    <NavBar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Sidebar from '../components/Navigation/Sidebar.vue'
+import NavBar from '../components/Navigation/NavBar.vue'
 
 export default {
   name: 'Home',
   components: {
     Sidebar,
+    NavBar
+
   }
 }
 </script>
